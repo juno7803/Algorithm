0a5442bb0eff65d106d0f2dcff0d46a2b6d267de
Juno <juno7803@naver.com>
2020-03-13 02:07:02 +0900

    Add QuickSort

---
+++ Math/quick sort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void quickSort(int* data, int start, int end);
+int main()
+{
+
+	return 0;
+}
+
+void quickSort(int* data, int start, int end)
+{
+	if (start >= end)
+	{
+		return;
+	}
+}
