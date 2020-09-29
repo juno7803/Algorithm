e7debfe6e5c4c5f781c2d1c812ffcf09afff9e5e
junholee <juno7803@naver.com>
2020-09-29 18:41:09 +0900

    자료구조: stack(10828) 풀이

---
+++ Math/Stack/10828.py
@@ -0,0 +1,43 @@
+class stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        if(self.items):
+            print(self.items.pop())
+        else:
+            print(-1)
+        
+    def empty(self):
+        if not self.items:
+            print(1)
+        else:
+            print(0)
+
+    def size(self):
+        print(len(self.items))
+
+    def top(self):
+        if(self.items):
+            print(self.items[int(len(self.items))-1])
+        else:
+            print(-1)
+
+n = int(input())
+stk = stack()
+
+for i in range(0,n):
+    op = list(input().split())
+    if(op[0] == "push"):
+        stk.push(op[1])
+    elif(op[0] == "pop"):
+        stk.pop()
+    elif(op[0] == "size"):
+        stk.size()
+    elif(op[0] == "empty"):
+        stk.empty()
+    elif(op[0] == "top"):
+        stk.top()
--- README.md
+++ README.md
@@ -5,3 +5,30 @@ Target : 1day per 1 problem!
 ## 알고리즘 공부 다시 시작(9/29~)
 사용 언어 변경 C++ -> Python
 하루에 한 문제 풀기
+
+### 자료구조
+STACK
+``` python3
+    class stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        if(self.isEmpty()):
+            return self.items.pop()
+        else:
+            return 0
+        
+    def isEmpty(self):
+        if not self.items:
+            return 1
+        else:
+            return 0
+
+    def size(self):
+        print(len(self.items))
+
+```
